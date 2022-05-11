8f654d90cf963fec29fa7595b4ddf5d9fdb86124
Isaac Palou <isaacpalou@paucasesnovescifp.cat>
2022-05-11 20:26:28 +0200

    feat :sparkles: new Class

---
+++ src/cat/paucasesnovescifp/herencia/personal/Alumne.java
@@ -0,0 +1,12 @@
+package cat.paucasesnovescifp.herencia.personal;
+
+public class Alumne extends Persona {
+	
+	public Alumne(String nom, String dataNaixament, String dni) {
+		super(nom, dataNaixament, dni);
+	}
+	
+	private String cicle;
+	private String curs;
+	
+}
---
+++ src/cat/paucasesnovescifp/herencia/personal/Professor.java
@@ -0,0 +1,13 @@
+package cat.paucasesnovescifp.herencia.personal;
+
+public class Professor extends Persona{
+
+	public Professor(String nom, String dataNaixament, String dni) {
+		super(nom, dataNaixament, dni);
+	}
+	
+	private String especialitat;
+	private int anyOposició;
+	
+	
+}
